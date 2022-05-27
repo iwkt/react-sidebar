c9e459ed9096998966e1f08fe7879e62b31088de
iwkt <iwk@mac.com>
2022-05-28 07:33:13 +0900

    add id

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-key */
 import React from 'react';
 import { SidebarData } from './SidebarData';
 
@@ -8,7 +7,14 @@ export const Sidebar = () => {
       <ul className="SidebarList">
         {SidebarData.map((value, key) => {
           return (
-            <li key={key} className="row">
+            <li
+              key={key}
+              className="row"
+              id={window.location.pathname == value.link ? 'active' : ''}
+              onClick={() => {
+                window.location.pathname = value.link;
+              }}
+            >
               <div id="icon">{value.icon}</div>
               <div id="title">{value.title}</div>
             </li>
